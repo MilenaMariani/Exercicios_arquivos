58690596b397c2290e62004a87058ad75e414f69
MilenaMariani <142273910+MilenaMariani@users.noreply.github.com>
2023-10-02 21:19:38 -0300

    Create exercicio_um.py

---
+++ exercicio_um.py
@@ -0,0 +1,7 @@
+with 
+estudante = {}
+
+nome_estudante =  (input("Digite o nome do estudante: "))
+idade_estudante = int (input("Digite a idade do estudante: "))
+curso_estudante = (input("Digite o curso do estudante: "))
+
