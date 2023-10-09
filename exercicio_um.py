91edae16e280d59f85530cd5056105975cbaea4d
MilenaMariani <142273910+MilenaMariani@users.noreply.github.com>
2023-10-09 19:47:29 -0300

    .

--- exercicio_um.py
+++ exercicio_um.py
@@ -1,7 +1,19 @@
-with 
-estudante = {}
+with open ('arquivo.txt', 'w') as arquivo:
+    estudante = {}
+    continuar = 1
 
-nome_estudante =  (input("Digite o nome do estudante: "))
-idade_estudante = int (input("Digite a idade do estudante: "))
-curso_estudante = (input("Digite o curso do estudante: "))
+    while continuar != 0:
+        nome_estudante = (input("Digite o nome do estudante: "))
+        idade_nome = (input("Digite a idade do estudante: "))     
+        curso_estudante = (input("Digite o curso do estudante: "))
+
+        estudante['nome'] = nome_estudante
+        estudante['idade'] = idade_nome
+        estudante['curso'] = curso_estudante
+
+        arquivo.write (f"{estudante}\n")
+
+        continuar = int (input("Digite 1 para continuar:\n Digite 0 para parar: "))
+        
+            
 
